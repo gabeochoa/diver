76d438ccae5f6388cded548eae5f898c789425bb
gabeochoa <gabeochoa@fb.com>
2022-01-22 15:25:02 -0500

    enemies are boids that try to follow you

--- diver/diver_layer.h
+++ diver/diver_layer.h
@@ -74,20 +74,112 @@ struct Player : public Movable {
 };
 
 struct Enemy : public Movable {
+    glm::vec2 velocity;
+    glm::vec2 acceleration;
+    float maxacc = 0.03f;
+
     Enemy(const glm::vec2& position_, const glm::vec2& size_, float angle_,
           const glm::vec4& color_, const std::string& textureName_)
         : Movable(position_, size_, angle_, color_, textureName_) {
-        speed = 1.f;
+        speed = 0.05f;
     }
 
-    void moveTowardPlayer(Time dt) {
+    glm::vec2 toPlayer(std::vector<std::shared_ptr<Enemy>>) {
         auto player = GLOBALS.get<Player>("player");
-        position = lerp(position, player.position, speed * dt);
+        return (player.position - position) / 100.f;
+    }
+
+    glm::vec2 separate(std::vector<std::shared_ptr<Enemy>> enemies) {
+        float desiredSep = glm::length(size) * 2.f;
+        glm::vec2 steer;
+        int count = 0;
+        for (auto e : enemies) {
+            float d = glm::distance(position, e->position);
+            if (d > 0 && d < desiredSep) {
+                steer += (glm::normalize(position - e->position) / d);
+                count++;
+            }
+        }
+        if (count) steer /= count;
+        if (glm::length(steer) > 0) {
+            steer = limit((glm::normalize(steer) * speed) - velocity, maxacc);
+        }
+        return steer;
+    }
+
+    glm::vec2 alignWith(std::vector<std::shared_ptr<Enemy>> enemies) {
+        float viewDist = glm::length(size) * 10.f;
+        glm::vec2 sum;
+        glm::vec2 steer;
+        int count = 0;
+        for (auto e : enemies) {
+            float d = glm::distance(position, e->position);
+            if (d > 0 && d < viewDist) {
+                sum += e->velocity;
+                count++;
+            }
+        }
+        if (count) {
+            sum /= count;
+            steer = limit((glm::normalize(sum) * speed) - velocity, maxacc);
+        }
+        return steer;
+    }
+
+    glm::vec2 seek(glm::vec2 target) {
+        auto desired = target - position;
+        return limit((glm::normalize(desired) * speed) - velocity, maxacc);
+    }
+
+    glm::vec2 cohesion(std::vector<std::shared_ptr<Enemy>> enemies) {
+        float viewDist = glm::length(size) * 10.f;
+        glm::vec2 sum;
+        int count = 0;
+        for (auto e : enemies) {
+            float d = glm::distance(position, e->position);
+            if (d > 0 && d < viewDist) {
+                sum += e->velocity;
+                count++;
+            }
+        }
+        if (count) {
+            sum /= count;
+            return seek(sum);
+        }
+        return glm::vec2{0.f};
+    }
+
+    void rules(std::vector<std::shared_ptr<Enemy>> enemies) {
+        auto player = toPlayer(enemies);
+        auto sep = separate(enemies);
+        auto align = alignWith(enemies);
+        auto unity = cohesion(enemies);
+
+        player *= 2.f;
+        sep *= 4.f;
+        align *= 1.f;
+        unity *= 1.f;
+
+        acceleration += player;
+        acceleration += sep;
+        acceleration += align;
+        acceleration += unity;
+    }
+
+    inline glm::vec2 limit(glm::vec2 v, float mx) {
+        if (glm::length(v) > mx) {
+            v = glm::normalize(v) * mx;
+        }
+        return v;
     }
 
     virtual void onUpdate(Time dt) {
         Movable::onUpdate(dt);
-        moveTowardPlayer(dt);
+        rules(EntityHelper::getEntitiesInRange<Enemy>(position, 10.f));
+        velocity += acceleration;
+        velocity = limit(velocity, speed);
+        position += velocity;
+        acceleration *= 0;
     }
     virtual const char* typeString() const { return "Enemy"; }
 };
@@ -136,13 +228,14 @@ struct DiverLayer : public Layer {
         EntityHelper::addEntity(player);
         GLOBALS.set<Player>("player", player.get());
 
-        for (int i = 0; i < 300; i++) {
+        for (int i = 0; i < 100; i++) {
             EntityHelper::addEntity(std::make_shared<Enemy>(Enemy(
-                glm::vec2{10 * cos(i), 10 * sin(i)}, glm::vec2{0.6f, 0.6f}, 0,
-                gen_rand_vec4(0.3f, 1.0f), peopleSprites[1])));
+                glm::vec2{randIn(1, 10) * cos(i), randIn(1, 10) * sin(i)},
+                glm::vec2{0.6f, 0.6f}, 0,
+                glm::vec4{gen_rand_vec3(0.3f, 1.0f), 1.f}, peopleSprites[1])));
         }
 
-        for (int i = 0; i < 300; i++) {
+        for (int i = 0; i < 100; i++) {
             EntityHelper::addEntity(
                 std::make_shared<StaticObject>(StaticObject(  //
                     glm::vec2{-10, i},                        //
